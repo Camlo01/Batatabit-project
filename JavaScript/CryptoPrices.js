76ec1e346df8bf48abd162031c30ffedc9024969
camlo01 <camilobeltran174@gmail.com>
2022-11-23 12:01:51 -0500

    Remplaced method for every request of each crypto coin | improved convertToHtml() | method fullUrl() added for return an url deppending what crypto do you want to find the price in what currency, too include the apiKey

--- JavaScript/CryptoPrices.js
+++ JavaScript/CryptoPrices.js
@@ -1,86 +1,84 @@
-// LLAVE Acceso CoinAPI.io = 3B1B6709-207F-46FC-B7D1-576A99BBF416
+// LLAVE Acceso CoinAPI.io = 12E42E01-F5AA-413C-AFAE-BA3180D99EE3
 //------------------------------------------
 // Nombres de Criptomonedas
 // Bitcoin = BTC
 // Ethereum = ETH
-// Tether = USDT
 // Bnb = BNB
+// Tether = USDT
 //------------------------------------------
 // Explicación Link
-// https://rest.coinapi.io/v1/exchangerate/{cryptoname}/{to exchange}?apikey=3B1B6709-207F-46FC-B7D1-576A99BBF416
+// `https://rest.coinapi.io/v1/exchangerate/{cryptoname}/{to exchange}?apikey=${apiKey}`
 // Peticion Precio BITCOIN en Dolares
-// https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=3B1B6709-207F-46FC-B7D1-576A99BBF416
+// `https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=${apiKey}`
 
-function getBTCPrice() {
-  setTimeout(() => {
-    fetch(
-      "https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=3B1B6709-207F-46FC-B7D1-576A99BBF416"
-    )
-      .then((response) => response.json())
-      .then(function (data) {
-        let precio = "$" + Math.trunc(data.src_side_base[0].rate);
-        console.log("Se inicializa el precio de BITCOIN en una variable");
-        document.getElementById("priceBitcoin").innerHTML = convertToHtml(precio);
-      })
-      .catch((error) => console.log("BITCOIN Se presentó el ERROR " + error));
-    document.getElementById("priceBitcoin").innerHTML = convertToHtml("$30.000");
-  }, 400);
-}
+import {
+  tooManyRequestsMessage,
+  errorMessage,
+  noDataMessage,
+  authenticationMessage,
+} from "./Messages.js";
 
-function getETHPrice() {
-  setTimeout(() => {
-    fetch(
-      "https://rest.coinapi.io/v1/exchangerate/ETH/USD?apikey=3B1B6709-207F-46FC-B7D1-576A99BBF416"
-    )
-      .then((response) => response.json())
-      .then(function (data) {
-        let precio = "$" + Math.trunc(data.src_side_base[0].rate);
-        console.log("Se inicializa el precio del ETHEREUM en una variable");
+/**
+ *Function that request the price of the crypto according
+ *to the currency and print it into the DOM
+ * @param {String} Crypto (BTC ejem.)
+ * @param {String} Currency (USD ejem.)
+ * @param {String} wherePrintIt (id of div in HTML)
+ */
+async function cryptoPrice(Crypto, Currency, wherePrintIt) {
+  // Get data
+  const url = fullUrl(Crypto, Currency);
+  const response = await fetch(url);
+  const wherePrintPrice = document.getElementById(wherePrintIt);
 
-        document.getElementById("priceEthereum").innerHTML = convertToHtml(precio);
-      })
-      .catch((error) => console.log(" ETHEREUM Se presentó el ERROR " + error));
-    document.getElementById("priceEthereum").innerHTML = convertToHtml("$1.800");
-  }, 800);
-}
-
-function getBNBPrice() {
-  setTimeout(() => {
-    fetch(
-      "https://rest.coinapi.io/v1/exchangerate/BNB/USD?apikey=3B1B6709-207F-46FC-B7D1-576A99BBF416"
-    )
-      .then((response) => response.json())
-      .then(function (data) {
-        let precio = "$" + Math.trunc(data.src_side_base[0].rate);
-        console.log("Se inicializa el precio del BNB");
-        document.getElementById("priceBnb").innerHTML = convertToHtml(precio);
-      })
-      .catch((error) => console.log(" BNB Se presentó el ERROR " + error));
-    document.getElementById("priceBnb").innerHTML = convertToHtml("$300");
-  }, 1200);
-}
-function getUSDTPrice() {
-  setTimeout(() => {
-    fetch(
-      "https://rest.coinapi.io/v1/exchangerate/USDT/USD?apikey=3B1B6709-207F-46FC-B7D1-576A99BBF416"
-    )
-      .then((response) => response.json())
-      .then(function (data) {
-        let tetherValue = data.rate;
-        let precio = "$" + tetherValue.toFixed(2);
-        console.log("Se inicializa el precio del TETHER en una variable");
-        document.getElementById("priceTether").innerHTML = convertToHtml(precio);
-      })
-      .catch((error) => console.log("TETHER Se presentó el ERROR " + error));
-    document.getElementById("priceTether").innerHTML = convertToHtml("$1");
-  }, 1600);
-}
-
-//Funcion imprimir precio en etiqueta parrafo
-function convertToHtml(precio) {
-  let parrafo = "<div>";
-  for (i = 0; i < 1; i++) {
-    parrafo += "<p>" + precio + "<p> </div>";
+  // What to do according the status code
+  switch (response.status) {
+    case 200:
+      const data = await response.json();
+      const price = `$${Math.trunc(data.rate)}`;
+      wherePrintPrice.innerHTML = convertToHtml(price);
+      break;
+    case 401:
+      authenticationMessage();
+      break;
+    case 429:
+      tooManyRequestsMessage();
+      wherePrintPrice.innerHTML = convertToHtml(`$!.!!!`);
+      break;
+    case 500:
+      errorMessage();
+      wherePrintPrice.innerHTML = convertToHtml(`$X.XXX`);
+      break;
+    case 550:
+      noDataMessage();
+      wherePrintPrice.innerHTML = convertToHtml(`$?.???`);
+      break;
   }
-  return parrafo;
+}
+
+/**
+ *Function that return code into HTML code
+ * @param {String} text
+ * @returns String of HTML code
+ */
+function convertToHtml(text) {
+  let htmlCode = `<div>`;
+  for (i = 0; i < 1; i++) {
+    htmlCode += `<p>${text}<p></div>`;
+  }
+  return htmlCode;
+  too;
+}
+
+/**
+ * Function that returns a String of the url to make the request
+ * @param {String} crypto
+ * @param {String} currency
+ * @returns full url
+ */
+
+function fullUrl(crypto, currency) {
+  const apiKey = `12E42E01-F5AA-413C-AFAE-BA3180D99EE3`;
+  const url = `https://rest.coinapi.io/v1/exchangerate/${crypto}/${currency}?apikey=${apiKey}`;
+  return url;
 }
